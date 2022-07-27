bbb153ab4781dcd28d5d808de1bbc8f39573e5c3
somramnani <som.ramnani@gmail.com>
2022-07-27 10:33:06 -0400

    add Header and styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+// import GoogleMapReact from "google-map-react";
+// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+// import Rating from "@material-ui/lab";
+// import useStyles from "./styles";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  return <div>Map</div>;
 };
 
 export default Map;
